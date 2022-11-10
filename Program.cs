76c5d022a1b6023cdc0269e07d4d7d1c33a87f73
hrishikesh kamble <hrishikamble13@gmail.com>
2022-11-10 17:15:54 +0530

    Welcome message

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Day24UserRegistrationLambda
+{
+    public class Program
+    {
+        public static void Main(string[]args)
+        {
+            Console.WriteLine("welcome to user registration using Lambda Expression");
+        }
+    }
+}
